6deee6d5d71d3b7299fd90f9b5c6e948f761eeec
Benjamin Barrois <ben.barrois@gmail.com>
2021-07-09 17:51:08 +0200

    ✨ Add ErrUnimplemented

--- errors/errors.go
+++ errors/errors.go
@@ -14,6 +14,7 @@ func (e *GenericError) Error() string { return fmt.Sprintf("%v: %s", e.errType, 
 
 var ErrNotFound = errors.New("not found")
 var ErrResourceExhausted = errors.New("resource exhausted")
+var ErrUnimplemented = errors.New("unimplemented")
 
 func Errorf(typ error, msg string, args ...interface{}) error {
 	ret := new(GenericError)
