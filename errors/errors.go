b9ce6a40c18f939d0fcb859c391899b482fb967d
Benjamin Barrois <ben.barrois@gmail.com>
2021-07-09 16:00:54 +0200

    ✨ Improve errors

--- errors/errors.go
+++ errors/errors.go
@@ -6,11 +6,18 @@ import (
 )
 
 type GenericError struct {
-	ErrType error
-	Msg     string
+	errType error
+	msg     string
 }
 
-func (e *GenericError) Error() string { return fmt.Sprintf("%v: %s", e.ErrType, e.Msg) }
+func (e *GenericError) Error() string { return fmt.Sprintf("%v: %s", e.errType, e.msg) }
 
 var ErrNotFound = errors.New("not found")
 var ErrResourceExhausted = errors.New("resource exhausted")
+
+func Errorf(typ error, msg string, args ...interface{}) error {
+	ret := new(GenericError)
+	ret.errType = typ
+	ret.msg = fmt.Sprintf(msg, args...)
+	return ret
+}
