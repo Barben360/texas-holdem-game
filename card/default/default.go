cd5f08080fd187252d817a1caa5f39274c8e46e4
Benjamin Barrois <ben.barrois@gmail.com>
2021-07-11 17:16:54 +0200

    ⚡️ Improve cards performance
    using const size slices

--- card/default/default.go
+++ card/default/default.go
@@ -6,7 +6,6 @@ import (
 	"sort"
 
 	"github.com/Barben360/texas-holdem-game/card"
-	"github.com/Barben360/texas-holdem-game/errors"
 )
 
 // Default is the default carder implementation
@@ -19,35 +18,16 @@ func New(ctx context.Context) (card.Carder, error) {
 }
 
 // Shuffle shuffles cards
-func (d *Default) Shuffle(ctx context.Context, cards card.Cards) error {
+func (d *Default) Shuffle52(ctx context.Context, cards card.Cards52) error {
 	rand.Shuffle(len(cards), func(i, j int) { cards[i], cards[j] = cards[j], cards[i] })
 	return nil
 }
 
-// Draw draws cards from top of pile. If not enough cards, draws all the cards and returns ErrResourceExhausted
-func (d *Default) Draw(ctx context.Context, quantity int, cards card.Cards) (drawn card.Cards, remaining card.Cards, err error) {
-	if quantity > len(cards) {
-		return cards, card.Cards{}, errors.Errorf(errors.ErrResourceExhausted, "not enough cards to draw")
-	}
-	return cards[:quantity], cards[quantity:], nil
-}
-
-// Sort cards sorts cards by rank first then by suit
-func (d *Default) Sort(ctx context.Context, cards card.Cards, descendent bool) error {
+func (d *Default) Sort5(ctx context.Context, cards card.Cards5, descendent bool) error {
 	if descendent {
-		sort.Sort(card.CardsDesc(cards))
+		sort.Sort(card.CardsDesc5(cards))
 	} else {
-		sort.Sort(card.CardsAsc(cards))
+		sort.Sort(card.CardsAsc5(cards))
 	}
 	return nil
-}
-
-// Less compares two cards by rank
-// If equal is true, then ranks are the same
-// If comp is false, then card1 > card2, otherwise card1 < card2. False if equal is true
-func (d *Default) Less(ctx context.Context, card1 card.Card, card2 card.Card) (equal bool, comp bool, err error) {
-	if card1.Rank == card2.Rank {
-		return true, false, nil
-	}
-	return false, card1.Rank < card2.Rank, nil
 }
--- card/models.go
+++ card/models.go
@@ -4,29 +4,31 @@ package card
 type CardSuit int8
 
 const (
-	CardSuit_Club    CardSuit = 0
-	CardSuit_Diamond CardSuit = 1
-	CardSuit_Hearts  CardSuit = 2
-	CardSuit_Spades  CardSuit = 3
+	CardSuit_None    CardSuit = 0
+	CardSuit_Club    CardSuit = 1
+	CardSuit_Diamond CardSuit = 2
+	CardSuit_Hearts  CardSuit = 3
+	CardSuit_Spades  CardSuit = 4
 )
 
 // CardRank is the rank of a card, ordered by value (as for poker)
 type CardRank int8
 
 const (
-	CardRank_2     CardRank = 0
-	CardRank_3     CardRank = 1
-	CardRank_4     CardRank = 2
-	CardRank_5     CardRank = 3
-	CardRank_6     CardRank = 4
-	CardRank_7     CardRank = 5
-	CardRank_8     CardRank = 6
-	CardRank_9     CardRank = 7
-	CardRank_10    CardRank = 8
-	CardRank_Jack  CardRank = 9
-	CardRank_Queen CardRank = 10
-	CardRank_King  CardRank = 11
-	CardRank_Ace   CardRank = 12
+	CardRank_None  CardRank = 0
+	CardRank_2     CardRank = 1
+	CardRank_3     CardRank = 2
+	CardRank_4     CardRank = 3
+	CardRank_5     CardRank = 4
+	CardRank_6     CardRank = 5
+	CardRank_7     CardRank = 6
+	CardRank_8     CardRank = 7
+	CardRank_9     CardRank = 8
+	CardRank_10    CardRank = 9
+	CardRank_Jack  CardRank = 10
+	CardRank_Queen CardRank = 11
+	CardRank_King  CardRank = 12
+	CardRank_Ace   CardRank = 13
 )
 
 // Card represents a card
@@ -36,23 +38,25 @@ type Card struct {
 }
 
 // Represents a list of cards, top of the pile first
-type Cards []Card
+type Cards5 [5]Card
+type Cards7 [7]Card
+type Cards52 [52]Card
 
-type CardsAsc Cards
-type CardsDesc Cards
+type CardsAsc5 Cards5
+type CardsDesc5 Cards5
 
-func (c CardsAsc) Len() int      { return len(c) }
-func (c CardsAsc) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
-func (c CardsAsc) Less(i, j int) bool {
+func (c CardsAsc5) Len() int      { return len(c) }
+func (c CardsAsc5) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c CardsAsc5) Less(i, j int) bool {
 	if c[i].Rank == c[j].Rank {
 		return c[i].Suit < c[j].Suit
 	}
 	return c[i].Rank < c[j].Rank
 }
 
-func (c CardsDesc) Len() int      { return len(c) }
-func (c CardsDesc) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
-func (c CardsDesc) Less(i, j int) bool {
+func (c CardsDesc5) Len() int      { return len(c) }
+func (c CardsDesc5) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c CardsDesc5) Less(i, j int) bool {
 	if c[i].Rank == c[j].Rank {
 		return c[i].Suit > c[j].Suit
 	}
