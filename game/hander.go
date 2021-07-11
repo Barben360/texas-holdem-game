b92ffcdcef383df6a3ab2a1500c06a56820adcc3
Benjamin Barrois <ben.barrois@gmail.com>
2021-07-11 17:21:52 +0200

    ✨ Add game base

--- game/hander.go
+++ game/hander.go
@@ -9,8 +9,8 @@ import (
 type Gamer interface {
 	// New creates a new game
 	New(ctx context.Context, nPlayers int8, nTokens Tokens, smallBlind Tokens, bigBlind Tokens) (Game, error)
-	// Reset resets a game object. Like New, but reusing all sub-slices
+	// Reset resets a game object. Like New, but reusing resources
 	Reset(ctx context.Context, g *Game, nPlayers int8, nTokens Tokens, smallBlind Tokens, bigBlind Tokens) error
-	// Play triggers an action from a player. Returned GameData can be safely accessed
+	// Play triggers an action from a player
 	Play(ctx context.Context, g *Game, playerId uuid.UUID, action Action) (GameData, error)
 }
--- game/models.go
+++ game/models.go
@@ -11,10 +11,10 @@ import (
 // Game represents a game
 type Game struct {
 	Id      uuid.UUID
-	Data    GameData   // Game data. Can be safely accessed contrary to other members
-	Deck    deck.Deck  // Game deck
-	Players Players    // Players
-	Table   card.Cards // Always five even if not all revealed
+	Data    GameData    // Game data. Can be safely accessed contrary to other members
+	Deck    deck.Deck   // Game deck
+	Players Players     // Players
+	Table   card.Cards5 // Always five even if not all revealed
 }
 
 type GameData struct {
@@ -42,11 +42,11 @@ const (
 // Player represents a player
 type Player struct {
 	Id          uuid.UUID
-	Hand        card.Cards // Hand (always 2 cards, ordered)
-	TokensOwned Tokens     // Owned tokens (does not count current bet)
-	TokensBet   Tokens     // Bet tokens
-	Rank        int8       // If 0, the player is still in game
-	HasFolded   bool       // If true, has folded at current round
+	Hand        card.Cards2 // Hand (always 2 cards, ordered)
+	TokensOwned Tokens      // Owned tokens (does not count current bet)
+	TokensBet   Tokens      // Bet tokens
+	Rank        int8        // If 0, the player is still in game
+	HasFolded   bool        // If true, has folded at current round
 }
 
 type Players []Player
@@ -54,8 +54,8 @@ type Players []Player
 type Tokens int
 
 type Action struct {
-	Type   ActionType
-	Amount Tokens // Only when playing, not folding
+	Type   ActionType // Action type
+	Amount Tokens     // Only when playing, not folding
 }
 
 type ActionType int8
@@ -64,60 +64,3 @@ const (
 	ActionType_Play ActionType = 0
 	ActionType_Fold ActionType = 1
 )
-
-type Hand struct {
-	Type          HandType
-	PrimaryRank   card.CardRank
-	SecondaryRank card.CardRank
-	Suit          card.CardSuit // Informational, for flushes only
-}
-
-// HandType represents hand type, ordered by ranking
-type HandType int8
-
-const (
-	HandType_SingleCard    HandType = 0
-	HandType_Pair          HandType = 1
-	HandType_TwoPair       HandType = 2
-	HandType_ThreeOfAKind  HandType = 3
-	HandType_Straight      HandType = 4
-	HandType_Flush         HandType = 5
-	HandType_FullHouse     HandType = 6
-	HandType_FourOfAKind   HandType = 7
-	HandType_StraightFlush HandType = 8
-	HandType_RoyalFlush    HandType = 9
-)
-
-func (h Hand) IsBetterThan(other Hand) (equal, better bool) {
-	if h.Type < other.Type {
-		return false, false
-	}
-	if h.Type > other.Type {
-		return false, true
-	}
-	// Same type
-	switch h.Type {
-	// All these cases depend only on primary rank (Three-of-a-kind and full house too, there can't be twice the same primary rank)
-	case HandType_StraightFlush, HandType_FourOfAKind, HandType_Flush,
-		HandType_Straight, HandType_Pair, HandType_ThreeOfAKind, HandType_FullHouse:
-		equal = h.PrimaryRank == other.PrimaryRank
-		if !equal {
-			better = h.PrimaryRank > other.PrimaryRank
-		}
-
-	// Two-pair case depends on primary and secondary rank
-	case HandType_TwoPair:
-		if h.PrimaryRank == other.PrimaryRank {
-			equal = h.SecondaryRank == other.SecondaryRank
-			if !equal {
-				better = h.SecondaryRank > other.SecondaryRank
-			}
-		} else {
-			better = h.PrimaryRank > other.PrimaryRank
-		}
-	// Two royal flushes are always equal
-	default:
-		equal = true
-	}
-	return equal, better
-}
